926162171aaf6f30285c55d4f1e672f7349fc368
henroben <ben@henroben.co.uk>
2016-10-05 12:09:30 +0100

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,33 +2,20 @@ var express = require('express');
 
 // Create app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    if(req.headers['x-forwarded-proto'] === 'http') {
+        // if http request, proceed
+        next();
+    } else {
+        // open weather api only returns over http, so redirect https requests
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 app.use(express.static('public'));
 
-//app.use(function(error, req, res, next) {
-//    if (error.syscall !== 'listen') {
-//        throw error;
-//    }
-//
-//    var bind = typeof port === 'string'
-//        ? 'Pipe ' + port
-//        : 'Port ' + port;
-//
-//    // handle specific listen errors with friendly messages
-//    switch (error.code) {
-//        case 'EACCES':
-//            console.error(bind + ' requires elevated privileges');
-//            process.exit(1);
-//            break;
-//        case 'EADDRINUSE':
-//            console.error(bind + ' is already in use');
-//            process.exit(1);
-//            break;
-//        default:
-//            throw error;
-//    }
-//});
-
-app.listen(3000, function() {
-    console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+    console.log('Express server is up on port ' + PORT);
 });
